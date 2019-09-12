6680808e9d2cbad5bf9f4ea651bdc39ba8192d13
Jeffrey Mcintyre <Flatlandpyhon67@hotmail.com>
2019-09-12 13:33:54 -0700

    imported elements object from base.js, added function for rendering

--- src/view/searchView.js
+++ src/view/searchView.js
@@ -1,4 +1,14 @@
+// below are named exports
 //export const func1 = (a,b) => console.log(`The sum is ${a + b}.`);
 //export const func2 = (z,y) => console.log(`The answer is ${z * y}`);
+import { elements } from "./base.js";
 
+export const getInput = () => elements.searchInput.value;
 
+const renderRecipe = () => {
+	
+}
+
+export const renderResults = recipes => {
+	recipes.forEach(renderRecipe)
+}
