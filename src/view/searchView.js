35f7766c9f0b433aaf879098a997020ba1c3dfd4
Jeffrey Mcintyre <Flatlandpyhon67@hotmail.com>
2019-09-17 14:21:01 -0700

    added markup for recipe list and functions for clearing content

--- src/view/searchView.js
+++ src/view/searchView.js
@@ -4,11 +4,31 @@
 import { elements } from "./base.js";
 
 export const getInput = () => elements.searchInput.value;
+export const clearInput = () => {
+	elements.searchInput.value = '';
+};
 
-const renderRecipe = () => {
-	
-}
+export const clearRecipeResults = () => {
+	elements.searchResList.innerHTML = '';
+};
 
-export const renderResults = recipes => {
-	recipes.forEach(renderRecipe)
-}
+const renderRecipe = recipe => {
+	const markup = `
+     <li>
+        <a class="results__link results__link--active" href="#${recipe.recipe_id}">
+            <figure class="results__fig">
+                <img src="${recipe.image_url}" alt="${recipe.title}">
+            </figure>
+            <div class="results__data">
+                <h4 class="results__name">${recipe.title}</h4>
+                <p class="results__author">${recipe.publisher}</p>
+            </div>
+        </a>
+    </li>
+	`;
+	elements.searchResList.insertAdjacentHTML('beforeend',markup);
+};
+
+export const renderResults = r => {
+	r.forEach(renderRecipe);
+};
