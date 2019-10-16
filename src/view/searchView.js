481c6eac3db410ab3868eedcdf5d1ebfa69e1de0
Jeffrey Mcintyre <Flatlandpyhon67@hotmail.com>
2019-10-16 09:53:46 -0700

    added render function

--- src/view/searchView.js
+++ src/view/searchView.js
@@ -12,6 +12,42 @@ export const clearRecipeResults = () => {
 	elements.searchResList.innerHTML = '';
 };
 
+const limitRecipeTitle = (title, limit = 17) => {
+	const newTitle = [];
+
+	if (title.length > limit) {
+		title.split(' ').reduce((acc, cur) => {
+			if (acc + cur.length <= limit) {
+				newTitle.push(cur);
+			}
+			return acc + cur.length;
+		}, 0);
+
+		return `${newTitle.join(' ')}...`;
+	}
+
+	return title;
+}
+
+const createButton = (page, type) => {
+
+};
+
+const renderButtons = (page, numResults, resPerPage) =>{
+	const pages = Math.ceil(numResults / resPerPage);
+
+	if(page === 1 && pages > 1){
+		// Button should go to next page
+	} else if(page < pages){
+		//Both button
+
+	} else if (page === pages && pages > 1){
+
+	}
+
+
+};
+
 const renderRecipe = recipe => {
 	const markup = `
      <li>
@@ -20,7 +56,7 @@ const renderRecipe = recipe => {
                 <img src="${recipe.image_url}" alt="${recipe.title}">
             </figure>
             <div class="results__data">
-                <h4 class="results__name">${recipe.title}</h4>
+                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                 <p class="results__author">${recipe.publisher}</p>
             </div>
         </a>
@@ -29,6 +65,8 @@ const renderRecipe = recipe => {
 	elements.searchResList.insertAdjacentHTML('beforeend',markup);
 };
 
-export const renderResults = r => {
-	r.forEach(renderRecipe);
+export const renderResults = (recipes, page = 1,  resultsPerPage = 10) => {
+	const start = (page - 1) * resultsPerPage;
+	const end = 10;
+	recipes.slice(start, end).forEach(renderRecipe);
 };
